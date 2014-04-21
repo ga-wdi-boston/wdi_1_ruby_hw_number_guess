fe491c0e1c8b941327eef2ca29e3a5fb93de3d6d
Lisa Plesko <plesko.l@gmail.com>
2014-04-21 18:34:43 -0400

    Add loop to keep track of number of guesses
    * Also fixed typo found in section if guess is greater than answer

--- guess.rb
+++ guess.rb
@@ -1,12 +1,14 @@
 # Generate a number between 1 and 10
 answer = rand(1..10)
+guess_count = 0
 
-# Ask the user to provide a number between 1 and 10
-print "Please guess a number between 1 and 10: "
-user_guess = gets.chomp.to_i
 
 # User is allowed 3 guesses
+while guess_count < 3
 
+  # Ask the user to provide a number between 1 and 10
+  print "Please guess a number between 1 and 10: "
+  user_guess = gets.chomp.to_i
 
 # Evaluate guess
   if user_guess == answer
@@ -14,18 +16,22 @@ user_guess = gets.chomp.to_i
 
   elsif user_guess < answer
     puts "You're a bit low, try again!"
+    guess_count = guess_count + 1
     # restart game if guesses < 3
     # exit if guesses = 3
 
   elsif user_guess > answer
-    puts "You're a big over, try again!"
+    puts "You're a bit over, try again!"
+    guess_count = guess_count + 1
     # restart game if guesses < 3
     # exit if guesses = 3
 
   else
     puts "Please resubmit a number between 1 and 10."
+    guess_count = guess_count + 1
   end
 
+end
 
 
-"Sorry, you're out of guesses.  You should have guessed.." #number
+puts "Sorry, you're out of guesses.  You should have guessed #{answer}"
