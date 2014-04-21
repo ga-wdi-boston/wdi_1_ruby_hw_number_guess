9cada65549bc04ca1003e1b01f8d06f867c1c18f
Lisa Plesko <plesko.l@gmail.com>
2014-04-21 18:22:25 -0400

    Game generates random number and evaluates it against user guess.
    * Game generates random number between inclusive range of 1 and 10
    * Insert simple if/else statement used to evaluate the first guess

--- guess.rb
+++ guess.rb
@@ -1,26 +1,30 @@
 # Generate a number between 1 and 10
-
+answer = rand(1..10)
 
 # Ask the user to provide a number between 1 and 10
+print "Please guess a number between 1 and 10: "
+user_guess = gets.chomp.to_i
+
 # User is allowed 3 guesses
 
 
 # Evaluate guess
-  # If guess is correct
-    "Congratulations!  You're a lucky guesser!"
+  if user_guess == answer
+    puts "Congratulations!  You're a lucky guesser!"
 
-  # If guess is < answer
-    "You're a bit low, try again!"
+  elsif user_guess < answer
+    puts "You're a bit low, try again!"
     # restart game if guesses < 3
     # exit if guesses = 3
 
-  # If guess is > answer
-    "You're a big over, try again!"
+  elsif user_guess > answer
+    puts "You're a big over, try again!"
     # restart game if guesses < 3
     # exit if guesses = 3
 
-  # else
-  "Please resubmit a number between 1 and 10."
+  else
+    puts "Please resubmit a number between 1 and 10."
+  end
 
 
 
