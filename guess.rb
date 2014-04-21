bbf63f84226c9c82fcf67153a735ee688627989c
Mike Talley <miketalley@hotmail.com>
2014-04-21 18:26:48 -0400

    Added suggestions for higher or lower for user

--- guess.rb
+++ guess.rb
@@ -9,19 +9,30 @@ puts "You will be given three tries to guess a random number between 1 and 10"
 puts "Good luck!"
 
 while !guessed && guesses_remaining > 0
-  print "You have #{guesses_remaining}! Please enter your guess: "
-  current_guess = gets
+  print "You have #{guesses_remaining} guesses remaining! Please enter your guess: "
+  current_guess = gets.to_i
   if current_guess == the_number
     guesses_remaining -= 1
     puts "CONGRATS! You have guesses the number!"
     puts "The number was #{the_number}!"
-    puts "It took you " + (3 - guesses_remaining).to_s + " to guess it!"
+    puts "It took you " + (3 - guesses_remaining).to_s + " try to guess it!"
     guessed = true
   else
     guesses_remaining -= 1
-    puts "Sorry! That is not the number. :("
+    puts "Sorry! That is not the number."
+    if the_number < current_guess
+      puts "The number is lower than " + current_guess.to_s
+    else
+      puts "The number is higher than " + current_guess.to_s
+    end
   end
 end
+
+if !guessed
+  puts "I'm sorry, the number was #{the_number}!"
+  puts "Better luck next time!"
+end
+
 
 
 
