3de8ff34b882d9696e9c2da2ea2fab05a6acec79
Kathleen Votolato <kvotolato3@gmail.com>
2014-04-21 18:02:22 -0400

    Tell User whether guess is correct or not

--- guess.rb
+++ guess.rb
@@ -4,6 +4,12 @@ random_number = rand(1..10)
 
 print "Please guess a random number between 1 and 10: "
 
-guess = gets.chomp
+guess = gets.chomp.to_i
+puts "Random number is #{random_number}"
 
+if guess == random_number
+  puts "You are correct!"
+else
+  puts "Doh! Better luck next time."
+end
 
