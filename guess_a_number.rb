8be0336d6725104ed9170f712025ffd2a907aaf9
Ethan Gould <ethan.g.907@gmail.com>
2014-04-21 18:52:56 -0400

    added number generator and prompt user for guess

--- guess_a_number.rb
+++ guess_a_number.rb
@@ -1,10 +1,5 @@
-def number_generate
-  correct_num = rand(10)
-  puts "I'm thinking of a number...can you guess it?"
-end
-
-def get_number
-  puts "Enter a guess"
-  guess = gets.chomp.to_i
-  puts guess
-end
+correct_num = rand(10)
+puts "I'm thinking of a number...can you guess it?"
+puts "Enter a guess"
+guess = gets.chomp.to_i
+puts guess
