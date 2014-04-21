7f4b5fdfda03778606b728c4f867e30e1220303c
Christine Lafontant <cklafontant@gmail.com>
2014-04-21 18:52:18 -0400

    First attempt at guessing the number added

--- guess.rb
+++ guess.rb
@@ -2,9 +2,16 @@
 
 random_number = rand(1..10)
 
-print "I am thinking of a number betweewn 1 and 10."
-print "Guess what it is:"
+puts "I am thinking of a number betweewn 1 and 10."
+puts "Guess what it is:"
 
 # User provides a guess and the number is assigned the variable guess_number
-guess_number = get.chomp
-print guess_number
+guess_number = gets.chomp
+puts guess_number
+
+# Response if user guesses correctly
+if guess_number == random_number
+  puts "Perfect!" + guess_number.to_s + " is correct!"
+else
+  puts "Try again: "
+end
