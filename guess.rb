b6aafd9de8d828e755e106f06c4ee228fbd1f3cc
Kathleen Votolato <kvotolato3@gmail.com>
2014-04-21 18:35:39 -0400

    Allow 3 guesses before revealing correct number.

--- guess.rb
+++ guess.rb
@@ -2,14 +2,27 @@
 
 random_number = rand(1..10)
 
-print "Please guess a random number between 1 and 10: "
+puts "Please guess a random number between 1 and 10."
+puts "You will have 3 chances."
+print "> "
+
 
 guess = gets.chomp.to_i
 puts "Random number is #{random_number}"
+guesses = 1
+
+while guess != random_number and guesses < 3
+  guesses += 1
+  puts "Guess again."
+  print ">"
+  guess = gets.chomp.to_i
+end
 
 if guess == random_number
   puts "You are correct!"
 else
   puts "Doh! Better luck next time."
+  puts "Correct number was #{random_number}."
 end
+
 
