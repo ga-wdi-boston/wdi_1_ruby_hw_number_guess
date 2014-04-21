f6bf37bb79aed4ae04b8f6c392ae94746799e3ee
Christine Lafontant <cklafontant@gmail.com>
2014-04-21 18:41:46 -0400

    Random number generated and guess number provided

---
+++ guess.rb
@@ -0,0 +1,10 @@
+# Generates the random number and assigns it to variable number
+
+random_number = rand(1..10)
+
+print "I am thinking of a number betweewn 1 and 10."
+print "Guess what it is:"
+
+# User provides a guess and the number is assigned the variable guess_number
+guess_number = get.chomp
+print guess_number
