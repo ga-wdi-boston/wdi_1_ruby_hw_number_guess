341bcc96226cd0b4733f6ba0aee336574668911f
Mike Talley <miketalley@hotmail.com>
2014-04-21 18:02:10 -0400

    Added random number generation between 1-10
    *Added intro text welcoming user

---
+++ guess.rb
@@ -0,0 +1,16 @@
+the_number = rand(10)+1
+guessed = false
+guesses_remaining = 3
+
+puts the_number
+
+puts "Welcome to Guess That Number!!"
+puts "You will be given three tries to guess a random number between 1 and 10"
+puts "Good luck!"
+
+
+
+
+
+
+
