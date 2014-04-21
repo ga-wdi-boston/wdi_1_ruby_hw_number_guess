ae6f915b863cb5693949288bfb99a71f08b903de
Christine Lafontant <cklafontant@gmail.com>
2014-04-21 19:06:16 -0400

    Provided response for user's second guess

--- guess.rb
+++ guess.rb
@@ -8,11 +8,23 @@ puts "Guess what it is:"
 # User provides a guess and the number is assigned the variable guess_number
 guess_number = gets.chomp
 
-# Response if user guesses correctly
+# Response to user's first guess
 if guess_number == random_number
   puts "Perfect!" + guess_number.to_s + " is correct!"
 elsif guess_number.to_i > random_number
-  puts "Try again. Think smaller this time."
+  puts "Try again. Think smaller this time. Second guess:"
 else  guess_number.to_i < random_number
-  puts "Try again. Think larger this time."
+  puts "Try again. Think larger this time. Second guess:"
+end
+
+# User provide second guess
+guess_number_2 = gets.chomp
+
+# Response to user's second guess
+if guess_number_2 == random_number
+  puts "Perfect!" + guess_number.to_s + " is correct!"
+elsif guess_number_2.to_i > random_number
+  puts "You are almost there. Think smaller this time. Second guess:"
+else  guess_number_2.to_i < random_number
+  puts "You are almost there. Think larger this time. Second guess:"
 end
