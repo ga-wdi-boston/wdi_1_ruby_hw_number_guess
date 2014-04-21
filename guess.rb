2ef7dd0f62f409ae37f0eecbc41b3f7519f6a20f
Mike Talley <miketalley@hotmail.com>
2014-04-21 18:49:05 -0400

    Added ability for user to input range to guess

--- guess.rb
+++ guess.rb
@@ -1,20 +1,27 @@
-the_number = rand(10)+1
+
 guessed = false
-guesses_remaining = 3
 current_guess = 0
 
-puts the_number
-
 puts "Welcome to Guess That Number!!"
-puts "You will be given three tries to guess a random number between 1 and 10"
+puts "You will be given a certain number of tries to guess a random number."
 puts "Good luck!"
+
+print "Please enter a maximum number to guess up to: "
+
+maximum_number = gets.to_i
+guesses_remaining = (maximum_number * 0.3).to_i
+max_guesses = guesses_remaining
+the_number = rand(maximum_number)+1
+
+puts "Based on the maximum number of #{maximum_number} you will have #{guesses_remaining} tries to guess the number."
+
 
 while !guessed && guesses_remaining > 0
   print "You have #{guesses_remaining} guesses remaining! Please enter your guess: "
   current_guess = gets.to_i
-  while current_guess > 11 || current_guess < 1
+  while current_guess > (maximum_number + 1) || current_guess < 1
     puts "You have entered an invalid number."
-    print "Please enter a number 1-10: "
+    print "Please enter a number 1-" + maximum_number.to_s + ": "
     current_guess = gets.to_i
   end
 
@@ -22,7 +29,7 @@ while !guessed && guesses_remaining > 0
     guesses_remaining -= 1
     puts "CONGRATS! You have guesses the number!"
     puts "The number was #{the_number}!"
-    puts "It took you " + (3 - guesses_remaining).to_s + " try to guess it!"
+    puts "It took you " + (max_guesses - guesses_remaining).to_s + " tries to guess it!"
     guessed = true
   else
     guesses_remaining -= 1
