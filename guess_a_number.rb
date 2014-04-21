e84fd496f2bbfcea841bb403ffa968b7dcf9513c
Ethan Gould <ethan.g.907@gmail.com>
2014-04-21 19:17:16 -0400

    added while loop to repeat code block until correct guess

--- guess_a_number.rb
+++ guess_a_number.rb
@@ -1,14 +1,17 @@
 correct_num = rand(10)
 puts "I'm thinking of a number...can you guess it?"
-puts "Enter a guess"
-guess = gets.chomp.to_i
 
-if guess == correct_num
-  puts "Congratualtions! You guessed the number correctly."
-elsif guess < correct_num
-  puts "Too low! Guess again."
-elsif guess > correct_num
-  puts "Too high! Guess gain."
-else
-  puts "Please enter a number."
+guess = nil
+while guess != correct_num do
+  puts "Enter a guess"
+  guess = gets.chomp.to_i
+  if guess == correct_num
+    puts "Congratualtions! You guessed the number correctly."
+  elsif guess < correct_num
+    puts "Too low! Guess again."
+  elsif guess > correct_num
+    puts "Too high! Guess gain."
+  else
+    puts "Please enter a number."
+  end
 end
