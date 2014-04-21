bc38de38b427240a859495eb97adf8c5697a64ad
Lisa Plesko <plesko.l@gmail.com>
2014-04-21 18:09:35 -0400

    Outline for number guessing game

---
+++ guess.rb
@@ -0,0 +1,27 @@
+# Generate a number between 1 and 10
+
+
+# Ask the user to provide a number between 1 and 10
+# User is allowed 3 guesses
+
+
+# Evaluate guess
+  # If guess is correct
+    "Congratulations!  You're a lucky guesser!"
+
+  # If guess is < answer
+    "You're a bit low, try again!"
+    # restart game if guesses < 3
+    # exit if guesses = 3
+
+  # If guess is > answer
+    "You're a big over, try again!"
+    # restart game if guesses < 3
+    # exit if guesses = 3
+
+  # else
+  "Please resubmit a number between 1 and 10."
+
+
+
+"Sorry, you're out of guesses.  You should have guessed.." #number
