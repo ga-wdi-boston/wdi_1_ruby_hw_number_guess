a362c5b2f58db98cb42eeb09d89747a280e68f39
Ethan Gould <ethan.g.907@gmail.com>
2014-04-21 18:58:19 -0400

    add conditional statement

--- guess_a_number.rb
+++ guess_a_number.rb
@@ -2,4 +2,13 @@ correct_num = rand(10)
 puts "I'm thinking of a number...can you guess it?"
 puts "Enter a guess"
 guess = gets.chomp.to_i
-puts guess
+
+if guess == correct_num
+  puts "Congratualtions! You guessed the number correctly."
+elsif guess < correct_num
+  puts "Too low! Guess again."
+elsif guess > correct_num
+  puts "Too high! Guess gain."
+else
+  puts "Please enter a number."
+end
