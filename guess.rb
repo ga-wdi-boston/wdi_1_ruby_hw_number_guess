a03817ea5b38542ae59e815358bd14c841b18962
Kathleen Votolato <kvotolato3@gmail.com>
2014-04-21 22:23:43 -0400

    Added validation

--- guess.rb
+++ guess.rb
@@ -3,13 +3,21 @@ random_number = rand(1..10)
 
 puts "Please guess a random number between 1 and 10."
 puts "You will have 3 chances."
-puts "Random number is #{random_number}"
 
 guesses = 1
 
 print "> "
 
 guess = gets.chomp.to_i
+
+
+def valid?(guess)
+  if guess > 10 || guess < 1
+    false
+  else
+    true
+  end
+end
 
 def hint(guess, random_number)
   if guess < random_number
@@ -19,21 +27,30 @@ def hint(guess, random_number)
   end
 end
 
-hint(guess, random_number)
-
 while guess != random_number and guesses < 3
-  guesses += 1
-  puts "Guess again."
-  print "> "
-  guess = gets.chomp.to_i
-  hint(guess, random_number)
+  valid = valid?(guess)
+  if valid == true
+    guesses += 1
+    hint(guess, random_number)
+    puts "Guess again."
+    print "> "
+    guess = gets.chomp.to_i
+
+  else
+    puts "Oops. The number has to be between 1 and 10. We won't count that."
+    puts "Guess again."
+    print "> "
+    guess = gets.chomp.to_i
+  end
 end
 
 if guess == random_number
-  puts "You are correct!"
+  puts "You are correct! Good job."
 else
-  puts "Doh! Better luck next time."
+  puts "Better luck next time!"
   puts "Correct number was #{random_number}."
 end
+
+
 
 
