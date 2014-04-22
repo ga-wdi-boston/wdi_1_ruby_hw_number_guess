bf950c2c338dd5cfca49710d2fe914789c19d85f
Amanda McDermott <amandapmcdermott@gmail.com>
2014-04-22 04:13:14 -0400

    Outputs an error w/o using up a guess when the number is out of range

--- guess.rb
+++ guess.rb
@@ -2,15 +2,18 @@ answer = 1 + rand(9)
 attempts = 1
 puts 'Guess a number from 1 to 10: '.chomp
 guess = gets.chomp.to_i
-puts answer
 
 while attempts <= 3
-  if guess < answer
-    puts 'Nope, try a greater number: '.chomp
+  if guess < 1 || guess > 10
+    puts 'That\'s out of range!'
+    puts 'Guess a number from 1 to 10: '.chomp
     guess = gets.chomp.to_i
-    attempts += 1
   elsif guess > answer
     puts 'Nope, try a smaller number: '.chomp
+    guess = gets.chomp.to_i
+    attempts += 1
+  elsif guess < answer
+    puts 'Nope, try a greater number: '.chomp
     guess = gets.chomp.to_i
     attempts += 1
   elsif guess == answer
