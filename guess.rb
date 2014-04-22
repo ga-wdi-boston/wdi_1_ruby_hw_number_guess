bbb3f0462f30fb12ec6fff0d8a391d047164f959
Amanda McDermott <amandapmcdermott@gmail.com>
2014-04-22 03:52:18 -0400

    Checks if guess is right, allows 3 guesses, shows answer.

--- guess.rb
+++ guess.rb
@@ -1,4 +1,18 @@
 answer = 1 + rand(9)
-puts "Guess a number from 1 to 10: ".chomp
-user_guess = gets.chomp.to_i
-puts answer
+max_attempts = 3
+attempts = 1
+puts 'Guess a number from 1 to 10: '.chomp
+guess = gets.chomp.to_i
+
+while attempts < 3
+  if guess != answer
+    puts 'Nope, try again: '.chomp
+    guess = gets.chomp.to_i
+    attempts += 1
+  elsif guess == answer
+    puts 'Congrats! You win'
+    break
+  end
+end
+
+puts 'You are out of tries! The right number was ' + answer.to_s
