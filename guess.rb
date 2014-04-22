ed04e925a37c36bea3114b28fd8f7b98fdf970c2
Amanda McDermott <amandapmcdermott@gmail.com>
2014-04-22 04:03:23 -0400

    Gives higher/lower hint on incorrect guess

--- guess.rb
+++ guess.rb
@@ -1,18 +1,23 @@
 answer = 1 + rand(9)
-max_attempts = 3
 attempts = 1
 puts 'Guess a number from 1 to 10: '.chomp
 guess = gets.chomp.to_i
+puts answer
 
-while attempts < 3
-  if guess != answer
-    puts 'Nope, try again: '.chomp
+while attempts <= 3
+  if guess < answer
+    puts 'Nope, try a greater number: '.chomp
+    guess = gets.chomp.to_i
+    attempts += 1
+  elsif guess > answer
+    puts 'Nope, try a smaller number: '.chomp
     guess = gets.chomp.to_i
     attempts += 1
   elsif guess == answer
     puts 'Congrats! You win'
     break
   end
+  if attempts > 3
+    puts 'You are out of tries! The right number was ' + answer.to_s + '.'
+  end
 end
-
-puts 'You are out of tries! The right number was ' + answer.to_s
