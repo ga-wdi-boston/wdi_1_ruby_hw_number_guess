1682cf730c3d3e36ef7dbcd679516ac4bc66ac03
kmccarthy623 <mccarthy.katie@gmail.com>
2014-04-21 23:08:27 -0400

    Program generates a random number and asks user to guess it.  If they number is correct user is told, if incorrect user guess again.

--- guess.rb
+++ guess.rb
@@ -1,9 +1,20 @@
 
 #Program generates a random number and asks user to guess it
-
 number_1 = rand(1..10)
 puts "Hi There! Guess a number between 1 and 10"
 number_1 = gets.chomp
+
+#If random number is guessed number, the program tells the user
+if number_1 == number_rand 
+	puts "Congratulations you guessed the number!"
+#If random number is not the guessed number
+
+elsif number_1 != number_rand
+	puts "Sorry, that is not the number"
+# The program tells the user to guess again and to enter a new number
+	puts "Guess again and enter a new number!"
+	number_2 = gets.chomp
+end
 
 
 
