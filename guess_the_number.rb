5c9a5afb21e12e803df0c3e49889de6cc47ee9bb
George Straehle <gstraehle@gmail.com>
2014-04-21 21:16:28 -0400

    allowed program to take one guess and exit

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -6,13 +6,21 @@ puts "You will only have three guesses, but I'll give you two hints."
 puts
 print "Make your first guess: "
 guess1 = gets.chomp #records the first response
-
+#validation loop here
 while (guess1.to_i < 1 || guess1.to_i > 10)
   puts "Your guess was not within range."
-  print "Please guess again"
+  print "Please guess again: "
   guess1 = gets.chomp
 end
-
-puts guess1
-puts randnum
-
+#temporary check-in to see if we're still running
+#puts guess1
+#puts randnum
+# a conditional starting point that only has one chance
+# win or lose you exit after your first guess... ip
+if guess1 == randnum
+  puts "Luck is on your side!"
+  exit
+else
+  puts "You lose!"
+  exit
+end
