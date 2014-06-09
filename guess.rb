e9acfbb40a7e43289d491c6b55a696bc4ec0dac1
csb324 <csb324@gmail.com>
2014-06-09 19:07:33 -0400

    Fix problem where it asks for the first guess twice

--- guess.rb
+++ guess.rb
@@ -3,10 +3,6 @@ max_number = 10
 magic_number = rand(max_number)
 max_guesses = 3
 
-# ask for a number
-puts "Guess a number, any number"
-guess = gets.chomp.to_i
-
 i = 0
 
 begin
@@ -14,7 +10,7 @@ begin
   guess = gets.chomp.to_i
 
   if guess == magic_number
-    puts "YOU'RE A WINNER!"
+    puts "YOU'RE A WINNER! The number was #{magic_number} Congratulations!"
     i = max_guesses
 
   elsif guess > magic_number
@@ -25,5 +21,6 @@ begin
     puts "That's a little too low"
     i += 1
   end
-end until i >= max_guesses
+
+end until i > max_guesses
 
