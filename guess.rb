12dabb07f873b1ba1737fa116f1e4b691ef40099
csb324 <csb324@gmail.com>
2014-06-09 19:02:52 -0400

    Add while loop
    * also, create max_guesses variable
    * and it works!

--- guess.rb
+++ guess.rb
@@ -1,19 +1,29 @@
 # establish the target number
-maximum = 10
-magic_number = rand(maximum)
+max_number = 10
+magic_number = rand(max_number)
+max_guesses = 3
 
 # ask for a number
 puts "Guess a number, any number"
 guess = gets.chomp.to_i
 
 i = 0
-if guess == magic_number
-  puts "YOU'RE A WINNER!"
-  i = 3
-elsif guess > magic_number
-  puts "That's a little too high"
-  i += 1
-elsif guess < magic_number
-  puts "That's a little too low"
-  i += 1
-end
+
+begin
+  puts "Guess a number, any number"
+  guess = gets.chomp.to_i
+
+  if guess == magic_number
+    puts "YOU'RE A WINNER!"
+    i = max_guesses
+
+  elsif guess > magic_number
+    puts "That's a little too high"
+    i += 1
+
+  elsif guess < magic_number
+    puts "That's a little too low"
+    i += 1
+  end
+end until i >= max_guesses
+
