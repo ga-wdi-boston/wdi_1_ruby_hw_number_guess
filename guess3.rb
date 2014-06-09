b580dd635c869247a89c3878c335a9f9ef980c23
EricRho <eric.rho.87@gmail.com>
2014-06-09 19:30:27 -0400

    GOT IT! Edited "if num_guess" code so if you guess right on the last try, it won't return fail status

--- guess3.rb
+++ guess3.rb
@@ -7,7 +7,7 @@ puts "Can you guess what it is?"
 loop do
   puts "Take a guess! "
   guess = gets.chomp.to_i
-  if num_guesses == 0
+  if num_guesses == 0 and guess != number
     puts "Nope, it was #{number}"
     break
   elsif guess == number
@@ -19,6 +19,5 @@ loop do
   elsif guess < number
       puts "No, higher"
       num_guesses -= 1
-    end
   end
-
+end
