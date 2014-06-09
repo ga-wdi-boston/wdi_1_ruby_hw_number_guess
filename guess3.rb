81de1be7346336ddcf1df83fa3341c8fb57aa789
EricRho <eric.rho.87@gmail.com>
2014-06-09 19:24:17 -0400

    Made it so  after incorrect attempts, game will end with the answer being given

--- guess3.rb
+++ guess3.rb
@@ -1,5 +1,5 @@
 number = rand(1..10)
-num_guesses = 3
+num_guesses = 2
 
 puts "I am thinking of a number between 1 and 10"
 puts "Can you guess what it is?"
@@ -7,8 +7,11 @@ puts "Can you guess what it is?"
 loop do
   puts "Take a guess! "
   guess = gets.chomp.to_i
-  if guess == number
-    puts "YOU GUESSED IT!"
+  if num_guesses == 0
+    puts "Nope, it was #{number}"
+    break
+  elsif guess == number
+    puts "You got it!"
     break
   elsif guess > number
       puts "No, lower"
@@ -16,10 +19,6 @@ loop do
   elsif guess < number
       puts "No, higher"
       num_guesses -= 1
-  else
-    if
-      num_guesses = 0
-      break
     end
   end
-end
+
