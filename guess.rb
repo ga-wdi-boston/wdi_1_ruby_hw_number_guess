7a8746d8aecd32e2ddb56390859764a7f9617026
krk324 <krk32488@gmail.com>
2014-06-10 09:26:40 -0400

    Add extra feature: diplays number of guesses

--- guess.rb
+++ guess.rb
@@ -1,4 +1,5 @@
 ## This Program will generate a random number and prompt the user to guess the number.
+puts
 puts "Welcome to Guess the Number Game!"
 
 # Prompts for random number's maximum.
@@ -6,6 +7,8 @@ print "Enter a maximum number you want to guess: "
 max_number = gets.chomp.to_i
 # Generates random number
 random_num = Random.rand(1...max_number)
+# Computes number of guess
+max_guess = (max_number*0.3).ceil
 
 # This method prompts user to enter a number.
 def input_number
@@ -13,7 +16,7 @@ def input_number
   @guess_num = gets.chomp.to_i
 end
 
-# This method prompts user to enter a number between 1-10 until they have a right number.
+# This method prompts user to enter a number between 1-max_number until they have a right number.
 def check_number(max_number)
   while @guess_num > max_number || @guess_num == 0
     input_number
@@ -22,7 +25,8 @@ end
 
 # Evaluate the guess and prompt the user if their guess is higher or lower than the actual number.
 i = 0
-while i < 3
+while i < max_guess
+  puts "You have #{max_guess} guess(es)."
   input_number
   check_number(max_number)
   if random_num == @guess_num
@@ -36,6 +40,7 @@ while i < 3
     puts
   end
   i += 1
+  max_guess -= 1
 end
 
 puts "The number was #{random_num}"
