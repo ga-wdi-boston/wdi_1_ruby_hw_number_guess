1695e1751b4ebc1ecd15862e97328ae82bcfd88a
krk324 <krk32488@gmail.com>
2014-06-10 08:41:43 -0400

    Modify check input method. Formatting.

--- guess.rb
+++ guess.rb
@@ -6,17 +6,17 @@ print "Enter a maximum number you want to guess: "
 max_number = gets.chomp.to_i
 # Generates random number
 random_num = Random.rand(1...max_number)
-puts random_num
 
 # This method prompts user to enter a number.
 def input_number
+  puts
   print "Guess a number between 1-10: "
   @guess_num = gets.chomp.to_i
 end
 
-# This method prompts user to enter a number between 1-10 until they have a right number.
+# This method prompts user to enter a number between 1-max_number until they have a right number.
 def check_number
-  while @guess_num > 10 || @guess_num == 0
+  while @guess_num > @max_number || @guess_num == 0
     input_number
   end
 end
@@ -31,10 +31,8 @@ while i < 3
     exit
   elsif random_num < @guess_num
     puts "The number is lower than your guess"
-    puts
   elsif random_num > @guess_num
     puts "The number is higher than your guess"
-    puts
   end
   i += 1
 end
