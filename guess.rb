665b3fe6b78b1927f42958e9da9ff6aaf7c48644
brimatteng <btenggren20@gmail.com>
2014-06-09 21:43:35 -0400

    program ends after three guesses and displays the correct guess.

--- guess.rb
+++ guess.rb
@@ -2,9 +2,6 @@
 
 num_to_guess = Random.new.rand(0..10).to_i
 
-print "Dear User: Please guess our random integer! ... "
-guess = gets.chomp
-
 def is_right? (gus, random_num)
   if gus.to_i == random_num
     true
@@ -13,14 +10,24 @@ def is_right? (gus, random_num)
   end
 end
 
-2.times do
+print "Dear User: Please guess our random integer! ... "
+wrong_guesses = 0
+
+3.times do
+  guess = gets.chomp
   if is_right?(guess,num_to_guess)
-    puts "Good guess!"
+    puts "Good guess! the number was: #{guess}"
     break
   else
+    wrong_guesses += 1
+    if wrong_guesses == 3
+      puts "Wrong!!!!! the correct number was: #{num_to_guess}"
+      break
+    end
     print "Guess again maybe: "
-    guess = gets.chomp
   end
 end
 
-puts num_to_guess
+
+
+
