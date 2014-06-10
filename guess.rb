a15e5adf3aa7403dc1ac46a0a7a1109d1328bdf0
Julie <juliesof@gmail.com>
2014-06-09 22:03:07 -0400

    Added features
    *increase rand num to 1 if the rand num is 0
    *create a while loop that allows the user to guess three times before revealing the answer

--- guess.rb
+++ guess.rb
@@ -1,1 +1,18 @@
 num = rand(11)
+num += 1 if num == 0
+guess_countr = 0
+
+puts "What number is Ruby thinking of, from 1 to 10?"
+guess = gets.chomp
+
+while guess_countr < 2
+  if guess == num
+    puts "You're right!"
+  else
+    puts "Nope, you're incorrect! Try again."
+    guess = gets.chomp
+    guess_countr += 1
+  end
+end
+
+puts "Sorry, dude. The number was #{num}"
