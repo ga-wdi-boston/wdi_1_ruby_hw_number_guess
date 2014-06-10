6f115c27c418da4af0dfc45e19657a16cf14feb8
Jake Austin <jakeaust07@gmail.com>
2014-06-09 21:32:25 -0400

    Add guess-again
    repeat until correct number is guessed, then exit with congrats

--- guess.rb
+++ guess.rb
@@ -7,9 +7,8 @@ correct_guess = rand(1..10)
 puts "Can you guess the number? : "
 user_guess = gets.chomp.to_i
 
-if correct_guess == user_guess
-  puts "You did it!!"
-else
-  puts "Wrong, too bad!"
+while(correct_guess != user_guess)
+  puts "Wrong, too bad! Try another guess:"
+  user_guess = gets.chomp.to_i
 end
-
+puts "You did it!!"
