4f10b37a13e2803fd0284702d4a8662495167100
Emma Gardner <emmagardner@outlook.com>
2014-06-09 23:25:04 -0400

    add feature that tells the user to guess higher or lower

--- guess.rb
+++ guess.rb
@@ -4,39 +4,33 @@ number = Random.new
 num_rand = number.rand(1..10)
 
 puts "Can you guess the random number between 1 and 10?"
-
 guess = gets.chomp.to_i
-
-# tell the user whether the guess was correct or not
 guess_count = 0
 
-
-while guess != num_rand && guess_count < 3
+while guess != num_rand && guess_count < 4
   if guess < 1 || guess > 10
-    puts "Your number is out of range. Guess again."
+    puts "ERROR- Your number is out of range. Guess again."
     guess = gets.chomp.to_i
   else
     guess_count =+ 1
-    puts "You're wrong! Guess again!"
-    guess = gets.chomp.to_i
+    if guess < num_rand
+      puts "You're wrong! The number is higher. Guess again!"
+      guess = gets.chomp.to_i
+    else guess > num_rand
+      puts "You're wrong! The number is lower. Guess again!"
+      guess = gets.chomp.to_i
+    end
+
   end
 end
-if guess == num_rand
-  puts "WOW! You guess correct!"
+
+if guess_count > 3
+  puts "You guessed 3 times. The number is #{num_rand}."
 end
-# allow the user to guess again if they guess incorrect
-=begin else
-# outputs an error and does not use up a guess when an out-of range number is entered
-  if guess < 1 || guess > 10
-    puts "Your guess is out of range. Please guess again."
-    guess = gets.chomp
-  else
-    while guess_count < 3
-    guess_count =+ 1
-    puts "Please guess again."
-    guess = gets.chomp
-    end
-=end
+
+if guess == num_rand
+  puts "WOW! You guessed correct!"
+end
 
 
 
@@ -44,7 +38,7 @@ end
 
 
 
-# gives a higher or lower hint on a n incorrect guess
 
-# the program ends and reveals the random number after 3 incorrect guesses
+
+
 
