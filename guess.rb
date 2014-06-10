2b72bbba20d9b45f1ab2d6026e3efbe8985088b9
dmcgann1 <dmcgann1@gmail.com>
2014-06-10 09:15:52 -0400

    Added some of the optional extra functionality prior to class

--- guess.rb
+++ guess.rb
@@ -1,14 +1,24 @@
+puts "Select a maximum number. Ill pick a number between 1 and your number and you have to guess the answer"
+
+#user picks max
+number = gets.chomp.to_i
+
 #Generates the random answer
-answer = rand(1..10)
+answer = rand(1..number)
+
+#Scales number of guesses based on 3 guesses per 10 numbers
+guesses = (number - 1) * 3/10
+
+puts "You have #{guesses} guesses"
 
 #initialises user_guess as != answer and counter
 user_guess = 0
 count = 0
 
-puts "Please select a number between 1 and 10"
+puts "Please select a number between 1 and #{number}"
 
 #While loop to run depending on critera of counter and as long as the user doesnt guess correctly
-while user_guess != answer && count < 3
+while user_guess != answer && count < guesses
   user_guess = gets.chomp.to_i
   if user_guess >= 1 && user_guess <= 10
     if user_guess == answer
