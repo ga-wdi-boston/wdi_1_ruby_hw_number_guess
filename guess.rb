355618af438ba26bb9adb5064c0a255e09a1908f
afeinber <alexandernfeinberg@gmail.com>
2014-06-09 21:58:08 -0400

    Checks if the user had put in a valid guess

--- guess.rb
+++ guess.rb
@@ -3,10 +3,17 @@ num_of_guesses = 3
 
 
 loop do
-  num_of_guesses -= 1
 
-  puts "What is the number you'd like to guess?"
-  user_guess = gets.chomp.to_i
+  begin
+    puts "What is the number you'd like to guess? Between 1 and 10 please."
+    user_guess = gets.chomp.to_i
+    unless user_guess > 0 && user_guess <= 10
+      puts "Invalid guess. Please make a guess between 1 and 10."
+    end
+  end while user_guess < 1 || user_guess > 10
+
+
+  num_of_guesses -= 1
 
   if user_guess == num_to_guess
     puts "Great that is right!"
@@ -14,7 +21,7 @@ loop do
   else
 
     if num_of_guesses == 0
-      puts "The number was #{num_to_guess}"
+      puts "Sorry. The number was #{num_to_guess}"
       break
     end
 
