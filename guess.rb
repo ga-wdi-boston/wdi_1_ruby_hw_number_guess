0eb16c83ef339ac53211268a99d23b146af0e585
Ben White <whtouche@gmail.com>
2014-06-09 23:12:07 -0400

    Delete commented out code

--- guess.rb
+++ guess.rb
@@ -5,17 +5,15 @@ i = 0
 while i < 3
   puts "I'm thinking of a number between 1 and 10, can you guess it?"
   guess = gets.chomp.to_i
-  # this works but is cheating
+  # this works but feels like cheating
   if guess == number
     puts "Yay!"
     i = 3
   elsif guess > number
     puts "Lower"
-    # guess = gets.chomp.to_i
     i = i+1
   else
     puts "Higher"
-    # guess = gets.chomp.to_i
     i = i+1
   end
 end
