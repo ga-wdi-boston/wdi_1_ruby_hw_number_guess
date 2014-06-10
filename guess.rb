cbdb4fbac4fc66c03e5682cfbf43434ba31683cb
Max Kohl <maxkohl810@gmail.com>
2014-06-09 21:31:12 -0400

    check to see if the user's guess is correct
    	* provides user with a success message if the values are equal
    	* provides user with a failure message if the values are not equal

--- guess.rb
+++ guess.rb
@@ -1,7 +1,13 @@
-random_number = Random.rand(1..10)
-
 puts "I'm thinking of a number between one and ten. I'll give you three chances to guess it!"
 guess = gets
+
+random_number = Random.rand(1..10)
+
+if random_number == guess.to_i
+  puts "You guessed correct! The number is indeed #{guess}."
+else
+  puts "That wasn't it, but you'll get it next time. I believe in you."
+end
 
 puts "You guessed #{guess}, and the correct answer is #{random_number}."
 
