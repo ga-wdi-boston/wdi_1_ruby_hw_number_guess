0dff7dd44492042e9977867598621f3191d48c3d
Max Kohl <maxkohl810@gmail.com>
2014-06-09 21:48:58 -0400

    ask user to guess again if their guess is incorrect

--- guess.rb
+++ guess.rb
@@ -3,13 +3,22 @@ guess = gets
 
 random_number = Random.rand(1..10)
 
-if random_number == guess.to_i
-  puts "You guessed correct! The number is indeed #{guess}."
-else
+wrong_answers = 0
+
+until random_number == guess.to_i
   puts "That wasn't it, but you'll get it next time. I believe in you."
+  puts "You guessed #{guess}, and the correct answer is #{random_number}."
+
+  puts "Guess again!"
+  guess = gets
+
+  puts "You guessed #{guess}, and the correct answer is #{random_number}."
+
+  puts "Guess again!"
+  guess = gets
 end
 
-puts "You guessed #{guess}, and the correct answer is #{random_number}."
+puts "You guessed correct! The number is indeed #{guess}."
 
 
 
