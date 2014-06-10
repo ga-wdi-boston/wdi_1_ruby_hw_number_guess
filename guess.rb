f945cc97453e5b547dc79d7b42f7ad56a9752308
Jodi Detch <jodidetch@gmail.com>
2014-06-09 22:17:15 -0400

    Program tells the user whether a guess was correct or not

--- guess.rb
+++ guess.rb
@@ -4,4 +4,6 @@ puts "Hello. I am a random number generator and I would like you to guess my
 next number. Please enter a number:"
 user_number = gets.chomp
 
-
+if user_number != rand(11)
+    puts "I'm sorry, that is incorrect."
+end
