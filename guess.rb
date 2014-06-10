ec0d77e013dfcc58fb7f90eb1e3a8597f9178308
brimatteng <btenggren20@gmail.com>
2014-06-09 21:56:28 -0400

    Change loop to while loop. Add higher or lower message. Added more variables

--- guess.rb
+++ guess.rb
@@ -1,6 +1,6 @@
 # For homework 1
-
-num_to_guess = Random.new.rand(0..10).to_i
+max_guess = 10
+num_to_guess = Random.new.rand(0..max_guess).to_i
 
 def is_right? (gus, random_num)
   if gus.to_i == random_num
@@ -10,19 +10,29 @@ def is_right? (gus, random_num)
   end
 end
 
-print "Dear User: Please guess our random integer! ... "
+print "Dear User: Please guess our random integer between 0 and #{max_guess} inclusively! ... "
 wrong_guesses = 0
+max_num_guesses = 3
+count = 0
 
-3.times do
-  guess = gets.chomp
+while count <= max_num_guesses
+  guess = gets.chomp.to_i
   if is_right?(guess,num_to_guess)
     puts "Good guess! the number was: #{guess}"
     break
+  elsif guess > max_guess || guess < 0
+    print "Number out of range try again..."
   else
     wrong_guesses += 1
+    count += 1
     if wrong_guesses == 3
       puts "Wrong!!!!! the correct number was: #{num_to_guess}"
       break
+    end
+    if guess > num_to_guess
+      puts "try lower"
+    else
+      puts "try higher"
     end
     print "Guess again maybe: "
   end
