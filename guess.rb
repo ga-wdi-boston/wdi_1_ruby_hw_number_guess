f45fdf1fd450b7f4d9f68aaa4ee8204f8f871fd1
testkitschen <katryn.mcintosh@gmail.com>
2014-06-09 22:24:51 -0400

    Out of range guess
    * Adding a response in case the guess is out of range.
    
    * This line makes me think that maybe I should use && for my high and low responses so that they aren't also triggered at the same time as the out of range respone. Are elsif's exclusive? I don't know / can't remember for sure so I will have to test it and see.
    
    * Out of range response needs to trigger a loop that goes back to guess1 and doesn't push them to guess2.

--- guess.rb
+++ guess.rb
@@ -1,13 +1,15 @@
 my_number = rand(9) + 1
 
-puts "I'm thinking of a number. What do you think it is?"
+puts "I'm thinking of a number between 1 and 10. I'll give you 3 chances to guess what it is. What is your first guess?"
 guess1 = gets.chomp
 
 if guess1 == my_number
   puts "You got it!"
 elsif guess1 > my_number
   puts "Try again. That guess was too high."
-else guess1 < my_number
+elsif guess1 < my_number
   puts "Try again. That guess was too low."
+elsif guess1 < 1 || guess1 > 10
+  puts "Woah! I asked for a number between 1 and 10. Don't worry, I won't count that guess against you. Guess again."
 end
 
