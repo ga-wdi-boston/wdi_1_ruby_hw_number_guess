6bb76bba4fefc8f2e8f6b8b493b927508efc891a
Jake Austin <jakeaust07@gmail.com>
2014-06-09 22:02:08 -0400

    Limit number of guesses, let user set upper bound

--- guess.rb
+++ guess.rb
@@ -3,12 +3,21 @@
 # homework 1
 # June 9, 2014
 
-correct_guess = rand(1..10)
+puts "What is the upper bound for guessing: "
+max_num = gets.chomp.to_i
+correct_guess = rand(1..max_num)
+num_chances = max_num / 10
 puts "Can you guess the number? : "
 user_guess = gets.chomp.to_i
-
-while(correct_guess != user_guess)
-  puts "Wrong, too bad! Try another guess:"
-  user_guess = gets.chomp.to_i
+#while(correct_guess != user_guess)
+while(num_chances > 0)
+  if user_guess == correct_guess
+    puts "You did it!!"
+    num_chances = -1
+  else
+    puts "Wrong, too bad! Try another guess -- #{num_chances.to_s} tries left: "
+    user_guess = gets.chomp.to_i
+    num_chances -= 1
+  end
 end
-puts "You did it!!"
+
