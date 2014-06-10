f79e6b1f2ce6d56be71431f5b73aa1e3f62bd658
sarahchristine913 <s.mele13@gmail.com>
2014-06-09 23:18:11 -0400

    Muck-filled commit!
    * Heavy commit due to lots of debugging and error methods
    * Reordered elements inside and outside of conditionals for logic
    * Conditional runs to subtract a guess for a valid entry, display remaining guesses and re-run conditional until no more guesses remain
    * Added conditional for random number reveal at end with sorry message

--- guess.rb
+++ guess.rb
@@ -1,20 +1,37 @@
 # random number generator with 1 to 10 range
 r = rand(1...11)
 
-# assigns value to number of guesses. Allows us to add, subtract or
-# make conditions as the value changes
-guesses = 3
 # Begins program user interaction with prompt for input
 puts "I'm thinking of a number between 1 and 10... You have 3 tries to guess it!"
 
 # assigns user number from input, converts to integer for boolean comparison
 user_number = gets.chomp.to_i
 
-# creates condition for proper number range
-# if input is out of range, error message puts and no guesses are lost
-if (user_number > 10 || user_number < 0)
-  puts "Error: Please try again.. Between 1 and 10, silly! You still have #{guesses}!"
-else
-  guesses -= 1
+# assigns value to number of guesses. Allows us to add, subtract or
+# make conditions as the value changes
+guesses = 3
+
+# runs as long as user has guesses left
+# first conditional checks first for valid number
+while guesses > 1
+  if (user_number == r)
+    puts "Congratulations, You guessed correctly!"
+    break
+  elsif (user_number > 10 || user_number < 0)
+    puts "Error: Please try again.. Between 1 and 10, silly! You still have #{guesses}!"
+  else
+    guesses -= 1
+    if (user_number > r)
+      puts "Incorrect, try again. (Guess lower!) #{guesses} left."
+    else (user_number < r)
+      puts "Incorrect, try again. (Guess higher!) #{guesses} left."
+    end
+  end
+  user_number = gets.chomp.to_i
 end
 
+
+# Add segment to run only if (user_number != r) at guesses = 0
+if guesses == 1 && (user_number != r)
+  puts "Good try, but you're out of guesses, the number I was thinking of is #{r}!"
+end
