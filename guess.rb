5d29426711254c198dca52d52cde211193a32a7e
sarahchristine913 <s.mele13@gmail.com>
2014-06-09 20:10:04 -0400

    Out-of-range error message
    * Created conditional that puts an error/reminder for out-of-range user number
    * Added guesses variable
    * Loop for taking away a guess for each in-range user number

--- guess.rb
+++ guess.rb
@@ -1,5 +1,21 @@
-r = rand(1...10)
+# random number generator with 1 to 10 range
+r = rand(1...11)
 
+# assigns value to number of guesses. Allows us to add, subtract or
+# make conditions as the value changes
+guesses = 3
+# Begins program user interaction with prompt for input
 puts "I'm thinking of a number between 1 and 10... You have 3 tries to guess it!"
 
-user_number = gets.chomp
+# assigns user number from input, converts to integer for boolean comparison
+user_number = gets.chomp.to_i
+
+# creates condition for proper number range
+# if input is out of range, error message puts and no guesses are lost
+for user_number in 1..10
+  if (user_number > 10 || user_number < 0) then
+    puts "Error: Please try again.. Between 1 and 10, silly! You still have #{guesses}!"
+    break
+  end
+  guesses -= 1
+end
