43310c776e54bb9db3264b845aa096491cf22987
Jake Austin <jakeaust07@gmail.com>
2014-06-09 22:33:11 -0400

    Fix out of range error message

--- guess.rb
+++ guess.rb
@@ -6,11 +6,11 @@
 puts "What is the upper bound for guessing: "
 max_num = gets.chomp.to_i
 correct_guess = rand(1..max_num)
-num_chances = max_num / 10
+num_chances = 3 + max_num / 10
 puts "Can you guess the number? : "
 user_guess = gets.chomp.to_i
 #while(correct_guess != user_guess)
-while(num_chances > 0)
+while(num_chances >= 0)
   if user_guess == correct_guess
     abort("You did it!!!")
   else
@@ -19,14 +19,15 @@ while(num_chances > 0)
     else
       hint = "lower..."
     end
-    puts "Wrong, too bad! Try again (hint: #{hint}) -- #{num_chances.to_s} tries left: "
-    user_guess = gets.chomp.to_i
-    num_chances -= 1
-  end
-  if !(1..max_num).include?(user_guess)
-    puts "Remember - 1 to #{max_num.to_s}, Try again:"
-    user_guess = gets.chomp.to_i
-    num_chances += 1
+
+    if !(1..max_num).include?(user_guess)
+      puts "Remember - 1 to #{max_num.to_s}, Try again:"
+      user_guess = gets.chomp.to_i
+    else
+      puts "Wrong, too bad! Try again (hint: #{hint}) -- #{num_chances.to_s} tries left: "
+      user_guess = gets.chomp.to_i
+      num_chances -= 1
+    end
   end
 end
 puts "The number was #{correct_guess.to_s}, better luck next time!"
