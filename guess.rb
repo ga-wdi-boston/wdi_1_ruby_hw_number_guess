e10827a24db8196577f1ec8a32e8d2eb724087aa
krk324 <krk32488@gmail.com>
2014-06-10 09:00:36 -0400

    Modify checkinput method to check the maximum number.

--- guess.rb
+++ guess.rb
@@ -9,14 +9,13 @@ random_num = Random.rand(1...max_number)
 
 # This method prompts user to enter a number.
 def input_number
-  puts
   print "Guess a number between 1-10: "
   @guess_num = gets.chomp.to_i
 end
 
-# This method prompts user to enter a number between 1-max_number until they have a right number.
-def check_number
-  while @guess_num > @max_number || @guess_num == 0
+# This method prompts user to enter a number between 1-10 until they have a right number.
+def check_number(max_number)
+  while @guess_num > max_number || @guess_num == 0
     input_number
   end
 end
@@ -25,16 +24,19 @@ end
 i = 0
 while i < 3
   input_number
-  check_number
+  check_number(max_number)
   if random_num == @guess_num
     puts "Congratulation! You guessed the right number!"
     exit
   elsif random_num < @guess_num
     puts "The number is lower than your guess"
+    puts
   elsif random_num > @guess_num
     puts "The number is higher than your guess"
+    puts
   end
   i += 1
 end
 
 puts "The number was #{random_num}"
+puts
