9aee3d6d0d7da6307d3c9ed36d3b8ce86e741e4a
brimatteng <btenggren20@gmail.com>
2014-06-10 09:51:47 -0400

    Adds spaces and fixes some mistakes in indentation

--- guess.rb
+++ guess.rb
@@ -16,17 +16,18 @@ end
 #Starts the game
 print "Dear User: Please guess our random integer between 0 and #{max_guess} inclusively! ... "
 wrong_guesses = 0
-#just something I thought of for number of guesses
-max_num_guesses = max_guess/5
+max_num_guesses = max_guess/5 #simple guess alloted logic
 count = 0
 
 # The game goes on
 while count <= max_num_guesses
   guess = gets.chomp.to_i
+
   #checks for a correct guess
   if is_right?(guess,num_to_guess)
     puts "Good guess! the number was: #{guess}"
     print "Would you like to play again? y/n "
+
     #checks for a yes and resets (Doesn't work right. Fell asleep before fixing)
     if gets.chomp.downcase == 'y'
       puts "generating a new number with same max"
@@ -36,9 +37,11 @@ while count <= max_num_guesses
     else
       break
     end
-  # checks to see if number is in range
+
+    # checks to see if number is in range
   elsif guess > max_guess || guess < 0
     print "Number out of range try again..."
+
   else
     # changes the count of wrong guesses and count of guesses
     wrong_guesses += 1
@@ -54,22 +57,18 @@ while count <= max_num_guesses
     # exits if they have run out of wrong guesses
     if wrong_guesses == max_num_guesses
       print "Wrong!!!!! the correct number was: #{num_to_guess} would you like to play again y/n? "
-        #resets if yes
-        if gets.chomp.downcase == 'y'
-          puts "generating a new number with same max"
-          count = 0
-          num_to_guess = Random.new.rand(0..max_guess).to_i
-          wrong_guesses = 0
-        else
-          break
-        end
+      #resets if yes
+      if gets.chomp.downcase == 'y'
+        puts "generating a new number with same max"
+        count = 0
+        num_to_guess = Random.new.rand(0..max_guess).to_i
+        wrong_guesses = 0
+      else
+        break
+      end
     end
 
     # starts the next turn
     print "Guess again: "
   end
 end
-
-
-
-
