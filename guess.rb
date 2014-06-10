fd453d9c40139db124afe4867033eda4eac5d954
afeinber <alexandernfeinberg@gmail.com>
2014-06-09 21:35:31 -0400

    Made the last commit actually work.

--- guess.rb
+++ guess.rb
@@ -1,15 +1,16 @@
-num_to_guess = Math.random
+num_to_guess = rand(1..10)
 num_of_guesses = 3
 
 while num_of_guesses > 0
-  num_of_guesses += 1
+  num_of_guesses -= 1
   puts "What is the number you'd like to guess?"
   user_guess = gets.chomp
 
-  if user_guess == num_to_guesses
+  if user_guess == num_to_guess
     puts "Great that is right!"
     break
   else
     puts "Sorry guess again."
   end
 end
+
