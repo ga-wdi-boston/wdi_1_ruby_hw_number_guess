47b7d02a70c5ff6c7fa4c92250c63246d69d9783
Max Kohl <maxkohl810@gmail.com>
2014-06-09 23:11:41 -0400

    tell user if random number is higher or lower than their guess

--- guess.rb
+++ guess.rb
@@ -1,17 +1,22 @@
 puts "I'm thinking of a number between one and ten. I'll give you three chances to guess it!"
-guess = gets
+guess = gets.to_i
 
 random_number = Random.rand(1..10)
 
 wrong_answers = 0
 
-until ((wrong_answers == 2) || (guess.to_i == random_number))
+until ((wrong_answers == 2) || (guess == random_number))
   wrong_answers += 1
-  puts "Guess again!"
-  guess = gets
+  case
+  when guess < random_number
+    puts "Guess again! Hint: go higher."
+  when guess > random_number
+    puts "Guess again! Hint: go lower."
+  end
+  guess = gets.to_i
 end
 
-if guess.to_i != random_number
+if guess != random_number
   puts "You are out of guesses! The correct answer is #{random_number}"
 else
   puts "You've guessed correctly!"
