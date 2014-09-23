bb70a8d0c9ed30c0d8617cfbd4ba380a08e606d4
Rebecca Paz <r.pazyaquian@gmail.com>
2014-09-23 14:07:13 -0400

    Fixed buggy behavior.

--- guess.rb
+++ guess.rb
@@ -1,8 +1,9 @@
 target_number = 1 + rand(9)
-guesses = 1
+guesses = 0
 
-def get_input
+def get_input(guesses)
   puts "Guess a number between 1 and 10."
+  puts "Tries remaining: #{3 - guesses}"
   guess = gets.chomp.to_i
   while guess < 1 || guess > 10
     puts "That's not in the right range! Try again."
@@ -11,25 +12,22 @@ def get_input
   guess
 end
 
-guessed_number = get_input
-
-begin
+while true do
+  guessed_number = get_input(guesses)
+  guesses += 1
   if guessed_number < target_number
     puts "The number is higher than that."
-    puts "Tries remaining: #{3 - guesses}"
-    guessed_number = get_input
   elsif guessed_number > target_number
     puts "The number's lower than that."
-    puts "Tries remaining: #{3 - guesses}"
-    guessed_number = get_input
-  else
+  elsif guessed_number == target_number
     puts "That's the correct number!"
+    break
   end
-  guesses += 1
   if guesses == 3
     puts "You're out of tries!"
+    break
   end
-end until guessed_number == target_number || guesses == 3
+end
 
 puts "The number was #{target_number}."
 puts "Game's over!"
