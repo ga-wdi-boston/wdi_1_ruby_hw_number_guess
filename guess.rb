76844a69251b70b26f15761cbc34d3dba92205cd
Holly Moody <hollymoody@charter.net>
2014-09-24 08:18:29 -0400

    Fix issue where not exiting program and clean up output.

--- guess.rb
+++ guess.rb
@@ -1,6 +1,9 @@
+require 'pry'
+
 # Would love to have feedback on how to make the code more succinct but still readable.
 # Also, would love feedback on ruby best practices
 highest_value = 10
+time_to_exit = false
 
 puts "Howdy!  Ready to play the number game?  Let's get started!!"
 puts "\n Please tell me your name => "
@@ -17,14 +20,14 @@ case highest_value
 end
 guesses_left = max_guesses
 num_guesses = 0
-puts "#{user_name.empty? ? '' : ' ' + user_name}, you will be allowed #{max_guesses} chances to guess a number between #{guess_range.first} and #{guess_range.last}."
+puts "#{user_name.empty? ? 'You' : ', ' + user_name + ', you'} will be allowed #{max_guesses} chances to guess a number between #{guess_range.first} and #{guess_range.last}."
 
 guess_this_number = Random.rand(guess_range)
-puts "Number to guess: #{guess_this_number}"
 
-# display message to enter number with #responses remaining
+# Loop until the user wants to exit the game
 begin
-  puts "\n\nLets roll#{user_name.empty? ? '' : ' ' + user_name}!! Please enter a number between #{guess_range.first} and #{guess_range.last} =>"
+  # display message to enter number with #responses remaining
+  puts "\n\nLets roll!! Please enter a number between #{guess_range.first} and #{guess_range.last} =>"
   user_guess = gets.chomp.to_i
 
   # TODO: Only convert to integer if user_guess is a numeric
@@ -34,27 +37,27 @@ begin
   if !guess_range.include?(user_guess)
     puts "Ohhh - sorry#{user_name.empty? ? '' : ' ' + user_name}, the value you specified (#{user_guess}) is not between #{guess_range.first} and #{guess_range.last}."
   elsif user_guess == guess_this_number
-    puts "\n\nCongratulations#{user_name.empty? ? '' : ' ' + user_name}!  You got the number in #{num_guesses} #{num_guesses  == 1 ? 'guess' : 'guesses'}!!"
+    puts "\n\nCongratulations#{user_name.empty? ? '' : ' ' + user_name}!  You got the number in #{num_guesses + 1} #{num_guesses  == 1 ? 'guess' : 'guesses'}!!"
   else
     higher = user_guess > guess_this_number
     guesses_left -= 1
     num_guesses = max_guesses - guesses_left
-    puts "Sorry#{user_name.empty? ? '' : ' ' + user_name}, your number(#{user_guess}) was #{higher ? 'higher' : 'lower'} than the number."
+    puts "Sorry#{user_name.empty? ? '' : ' ' + user_name}, the number #{user_guess} was too #{higher ? 'high' : 'low'}."
     if guesses_left == 0
       puts "So sorry#{user_name.empty? ? '' : ' ' + user_name}, you've run out of guesses. The number we are trying to guess is #{guess_this_number}"
     else
-      puts "Try again - you've got #{guesses_left} guesses left!"
+      puts "Try again - you've got #{guesses_left} #{guesses_left  == 1 ? 'guess' : 'guesses'} left!"
     end
   end
 
-
   # Determine if it is time to exit the loop - still not quite working
-  time_to_exit = (user_guess == guess_this_number) or (guesses_left <= 0)
+  time_to_exit = ((user_guess == guess_this_number) or (guesses_left <= 0))
   if time_to_exit
-    puts "\n\n#{user_name}, do you want to play again?? (y or n)"
+    puts "\n\nDo you want to play again?? (y or n)"
     if gets.chomp == 'y'
       guess_this_number = Random.rand(guess_range)
-      guesses_left = 1
+      guesses_left = max_guesses
+      num_guesses = 0
       time_to_exit = false
     end
   end
